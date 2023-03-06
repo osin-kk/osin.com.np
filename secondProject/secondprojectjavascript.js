7256335d81cd9007a7f419e60c4f7c864de1b2ba
osin-kk <91796036+osin-kk@users.noreply.github.com>
2023-03-06 20:43:31 +1100

    Update secondprojectjavascript.js

--- secondProject/secondprojectjavascript.js
+++ secondProject/secondprojectjavascript.js
@@ -1,62 +1,25 @@
-const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-document.querySelector("h1").onmouseover = event => {
+const body = document.querySelector("#body");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function animateHeading(heading) {
   let iterations = 0;
-  
   const interval = setInterval(() => {
-  event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-    if(index < iterations) {
-      return event.target.dataset.value[index];
+    heading.innerText = heading.innerText.split("").map((letter, index) => {
+      if(index < iterations) {
+        return heading.dataset.value[index];
+      }
+      return letters[Math.floor(Math.random() * 26)];
+    }).join("");
+    
+    if(iterations >= heading.dataset.value.length) {
+      clearInterval(interval);
     }
-    
-    return letters[Math.floor(Math.random() * 26)];
-}).join("");
-
-
-  if(iterations >= event.target.dataset.value.length) { clearInterval(interval);
-                                                      }
-    
-  iterations +=1;
-}, 90);
+    iterations += 1;
+  }, 90);
 }
 
-const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-document.querySelector("h2").onmouseover = event => {
-  let iterations = 0;
-  
-  const interval = setInterval(() => {
-  event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-    if(index < iterations) {
-      return event.target.dataset.value[index];
-    }
-    
-    return letters[Math.floor(Math.random() * 26)];
-}).join("");
-
-
-  if(iterations >= event.target.dataset.value.length) { clearInterval(interval);
-                                                      }
-    
-  iterations +=1;
-}, 90);
-}
-
-const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-document.querySelector("h3").onmouseover = event => {
-  let iterations = 0;
-  
-  const interval = setInterval(() => {
-  event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-    if(index < iterations) {
-      return event.target.dataset.value[index];
-    }
-    
-    return letters[Math.floor(Math.random() * 26)];
-}).join("");
-
-
-  if(iterations >= event.target.dataset.value.length) { clearInterval(interval);
-                                                      }
-    
-  iterations +=1;
-}, 90);
-}
+body.addEventListener("mouseover", function(event) {
+  if (event.target.matches("h1, h2, h3")) {
+    animateHeading(event.target);
+  }
+});
