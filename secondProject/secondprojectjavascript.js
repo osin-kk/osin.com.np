38650e3eefbd0df64355ebf468092f0209ba315c
Osin Khatiwada <osinkhatiwada@gmail.com>
2023-03-06 21:16:06 +1100

    updated javasccrript

--- secondProject/secondprojectjavascript.js
+++ secondProject/secondprojectjavascript.js
@@ -1,25 +1,23 @@
-const body = document.querySelector("#body");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function animateHeading(heading) {
-  let iterations = 0;
-  const interval = setInterval(() => {
-    heading.innerText = heading.innerText.split("").map((letter, index) => {
-      if(index < iterations) {
-        return heading.dataset.value[index];
-      }
-      return letters[Math.floor(Math.random() * 26)];
-    }).join("");
-    
-    if(iterations >= heading.dataset.value.length) {
-      clearInterval(interval);
-    }
-    iterations += 1;
-  }, 90);
-}
+for (let i = 1; i <= 3; i++) {
+  const heading = document.querySelector(`h${i}`);
+  heading.addEventListener("mouseover", event => {
+    let iterations = 0;
 
-body.addEventListener("mouseover", function(event) {
-  if (event.target.matches("h1, h2, h3")) {
-    animateHeading(event.target);
-  }
-});
+    const interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+        if (index < iterations) {
+          return event.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      }).join("");
+
+      if (iterations >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iterations += 1;
+    }, 90);
+  });
+}
