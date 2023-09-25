22952137fda61b98d27824b4a338bf54d1916ac0
osin-kk <91796036+osin-kk@users.noreply.github.com>
2023-09-25 17:28:50 +1000

    Update index[1].js

--- index[1].js
+++ index[1].js
@@ -1,109 +1,16 @@
-//
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Get a reference to the project container
+  const projectContainer = document.querySelector(".project-container");
 
-  // animation
-  function initAnimation() {
-    animate();
-    for (var i in points) {
-      shiftPoint(points[i]);
-    }
-  }
+  // Define the project URL
+  const projectUrl = "osin.com.np/newp.html";
 
-  function animate() {
-    if (animateHeader) {
-      ctx.clearRect(0, 0, width, height);
-      for (var i in points) {
-        // detect points in range
-        if (Math.abs(getDistance(target, points[i])) < 4000) {
-          points[i].active = 0.3;
-          points[i].circle.active = 0.6;
-        } else if (Math.abs(getDistance(target, points[i])) < 20000) {
-          points[i].active = 0.1;
-          points[i].circle.active = 0.3;
-        } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-          points[i].active = 0.02;
-          points[i].circle.active = 0.1;
-        } else {
-          points[i].active = 0;
-          points[i].circle.active = 0;
-        }
+  // Create a link element for the project
+  const projectLink = document.createElement("a");
+  projectLink.href = projectUrl;
+  projectLink.textContent = "View Project"; // Change the text as needed
 
-        drawLines(points[i]);
-        points[i].circle.draw();
-      }
-    }
-    requestAnimationFrame(animate);
-  }
-
-  function shiftPoint(p) {
-    TweenLite.to(p, 1 + 1 * Math.random(), {
-      x: p.originX - 50 + Math.random() * 100,
-      y: p.originY - 50 + Math.random() * 100,
-      ease: Circ.easeInOut,
-      onComplete: function () {
-        shiftPoint(p);
-      }
-    });
-  }
-
-  // Canvas manipulation
-  function drawLines(p) {
-    if (!p.active) return;
-    for (var i in p.closest) {
-      ctx.beginPath();
-      ctx.moveTo(p.x, p.y);
-      ctx.lineTo(p.closest[i].x, p.closest[i].y);
-      ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
-      ctx.stroke();
-    }
-  }
-
-  function Circle(pos, rad, color) {
-    var _this = this;
-
-    // constructor
-    (function () {
-      _this.pos = pos || null;
-      _this.radius = rad || null;
-      _this.color = color || null;
-    })();
-
-    this.draw = function () {
-      if (!_this.active) return;
-      ctx.beginPath();
-      ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
-      ctx.fill();
-    };
-  }
-
-  // Util
-  function getDistance(p1, p2) {
-    return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-  }
-)();
-// Smooth Scrolling for Navigation
-document.addEventListener("DOMContentLoaded", function() {
-  const navLinks = document.querySelectorAll("#fixed-nav a");
-  navLinks.forEach(link => {
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
-      targetElement.scrollIntoView({ behavior: "smooth" });
-    });
-  });
-
-  // Placeholder logic for Project Showcase
-  const prevButton = document.getElementById("prev-project");
-  const nextButton = document.getElementById("next-project");
-
-  prevButton.addEventListener("click", function() {
-    // Logic for showing the previous project
-    console.log("Previous button clicked");
-  });
-
-  nextButton.addEventListener("click", function() {
-    // Logic for showing the next project
-    console.log("Next button clicked");
-  });
+  // Append the link element to the project container
+  projectContainer.appendChild(projectLink);
 });
