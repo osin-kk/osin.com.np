ee5df34c04a2d5c103a692d9fb1b3d1cc2de6cf2
osin-kk <91796036+osin-kk@users.noreply.github.com>
2023-10-11 10:26:25 +1100

    Update index[1].js

--- index[1].js
+++ index[1].js
@@ -1,16 +1,1 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Get a reference to the project container
-  const projectContainer = document.querySelector(".project-container");
 
-  // Define the project URL
-  const projectUrl = "newp.html";
-
-  // Create a link element for the project
-  const projectLink = document.createElement("a");
-  projectLink.href = projectUrl;
-  projectLink.textContent = "View Project"; // Change the text as needed
-
-  // Append the link element to the project container
-  projectContainer.appendChild(projectLink);
-});
