87c7b5c913da89ea3ef5fbc271cb16234d00cdc3
Osin Khatiwada <91796036+osin-kk@users.noreply.github.com>
2023-10-16 16:28:52 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const wordList = ["APPLE", "LEMON", "ORANGE", "BANANA", "GRAPE"];
-    const maxAttempts = 6;
-    let targetWord = getRandomWord(wordList);
-    let attemptsLeft = maxAttempts;
-    
-    const wordDisplay = document.getElementById('word-display');
-    const guessInput = document.getElementById('guess-input');
-    const guessButton = document.getElementById('guess-button');
-    const attemptsLeftDisplay = document.getElementById('attempts-left');
-    const messageDisplay = document.getElementById('message');
-    
-    // Initialize the word display with underscores
-    let guessedWord = "_".repeat(targetWord.length);
-    wordDisplay.textContent = guessedWord;
-    
-    // Function to get a random word from the list
-    function getRandomWord(wordList) {
-        const randomIndex = Math.floor(Math.random() * wordList.length);
-        return wordList[randomIndex];
-    }
-    
-    // Function to check if the guess is correct
-    function checkGuess(guess) {
-        let newGuessedWord = '';
-        for (let i = 0; i < targetWord.length; i++) {
-            if (guess === targetWord[i]) {
-                newGuessedWord += guess;
-            } else {
-                newGuessedWord += guessedWord[i];
-            }
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (let i = 1; i <= 3; i++) {
+  const heading = document.querySelector(`h${i}`);
+  heading.addEventListener("mouseover", event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+        if (index < iterations) {
+          return event.target.dataset.value[index];
         }
-        return newGuessedWord;
-    }
-    
-    // Event listener for the guess button
-    guessButton.addEventListener('click', () => {
-        const guess = guessInput.value.toUpperCase();
-        if (guess.length !== 1 || !/[A-Z]/.test(guess)) {
-            messageDisplay.textContent = 'Please enter a valid single letter guess.';
-            return;
-        }
-        
-        if (targetWord.includes(guess)) {
-            guessedWord = checkGuess(guess);
-            wordDisplay.textContent = guessedWord;
-            if (guessedWord === targetWord) {
-                messageDisplay.textContent = 'Congratulations! You guessed the word!';
-                guessInput.disabled = true;
-            }
-        } else {
-            attemptsLeft--;
-            attemptsLeftDisplay.textContent = `Attempts Left: ${attemptsLeft}`;
-            if (attemptsLeft === 0) {
-                messageDisplay.textContent = `Sorry, you've run out of attempts. The word was ${targetWord}.`;
-                guessInput.disabled = true;
-            }
-        }
-        
-        guessInput.value = '';
-    });
-});
+        return letters[Math.floor(Math.random() * 26)];
+      }).join("");
+
+      if (iterations >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iterations += 1;
+    }, 90);
+  });
+}
+const blob = document.getElementById("blob");
+document.body.onpointermove = event => {
+  const { clientX, clientY } =event;
+  blob.animate({
+    left : `${clientX}px`,
+    top : `${clientY}px`
+  },{ duration: 3000, fill: "forwards"});
+}
